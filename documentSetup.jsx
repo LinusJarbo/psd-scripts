dfa0c78c38ac8986dd74c46d223a1e0bbeef2824
Linus <lj@hauntedhouse.se>
2018-03-31 13:55:17 +0200

    Added document
    Added documnet setup script and info in readme file

---
+++ documentSetup.jsx
@@ -0,0 +1,39 @@
+﻿//if you don't need to review the image content, there might be faster methods outside photoshop to setup document?
+// tested in photoshop CC 19.1.12
+
+//Does:
+//This sets a document mode to RGB, 300 dpi without rescaling (and to color profile Adobe RGB(1998) if you switch the false statement to true)
+
+//Usage:
+//run this script from an action, and run that action from a hotkey set to run selected action
+
+#target photoshop
+
+var doc = app.activeDocument;
+
+//set color mode to RGB
+if(doc.mode != DocumentMode.RGB) {
+       alert("doc mode is not RGB - will set to RGB");
+       doc.changeMode(ChangeMode.RGB);
+}
+
+//set resolution only, no resample
+if(doc.resolution != 300){
+    alert("doc resolution is not 300 - will set to 300");
+    doc.resizeImage(undefined, undefined, 300, ResampleMethod.NONE);
+ };
+
+//set colorprofile
+if(doc.colorProfileName != "Adobe RGB (1998)"){
+    alert("wrong profile");
+   
+   //set to true if you trust the convertProfile function.
+   if(false){
+       var intent = "PERCEPTUAL";
+        var blackPointCompensation = true;
+        var dither = true;
+        doc.convertProfile ("Adobe RGB (1998)", Intent.RELATIVECOLORIMETRIC, blackPointCompensation, dither); 
+    }
+};
+
+alert(doc.colorProfileName);
